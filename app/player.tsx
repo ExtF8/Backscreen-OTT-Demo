0c523b3ce721f2dfd485075a6515eeaa720f8e31
ExtF8 <78138567+ExtF8@users.noreply.github.com>
2025-08-19 15:51:35 +0300

    Add basic screen with movie lookup by id

--- app/player.tsx
+++ app/player.tsx
@@ -1,6 +1,27 @@
+import { useLocalSearchParams } from 'expo-router';
 import { StyleSheet, Text, View } from 'react-native';
+import { Movie } from '../types/Movies';
+import { getMovies } from '../utils/catalog';
 
 export default function PlayerScreen() {
+    const { id } = useLocalSearchParams<{ id: string }>();
+
+    const movies: Movie[] = getMovies();
+    const movie = movies.find(movie => movie.id === id);
+
+    // Handle not found
+    if (!movie) {
+        return (
+            <View style={styles.center}>
+                <Text style={styles.error}>Movie not found</Text>
+            </View>
+        );
+    }
+
+    // Local playback state
+
+    // Render video
+    // Loading and errors
     return (
         <View style={styles.container}>
             <Text>This will be a player screen.</Text>
@@ -11,7 +32,17 @@ export default function PlayerScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: '#000000',
+    },
+    center: {
+        flex: 1,
+        backgroundColor: 'black',
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    error: {
+        marginBottom: 12,
+        textAlign: 'center',
+        color: '#b6b6b6',
     },
 });
