6308eceb4532e04b41a3025ddbeb8c675b1659e0
ExtF8 <78138567+ExtF8@users.noreply.github.com>
2025-08-19 10:15:32 +0300

    Add Movie type

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import localCatalog from '@/data/catalog.json';
+import { Movie } from '@/types/Movies';
+
 
 export default function HomeScreen() {
+    const [focusedId, setFocusedId] = useState<string | null>(null);
+
     return (
         <View style={styles.container}>
             <Text>This will be a Home screen.</Text>
+            <FlatList 
+                data={catalog}
+                numColumns={3}
+                keyExtractor={({ item }) => item.id}
+            />
         </View>
     );
 }
---
+++ types/Movies.ts
@@ -0,0 +1,9 @@
+// Movie type
+export type Movie = {
+    id: string;
+    title: string;
+    description: string;
+    thumbnail: string;
+    streamUrl: string;
+    duration: number;
+}
