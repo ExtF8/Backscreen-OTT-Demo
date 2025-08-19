2fb9061903ff79349038f812b30fa662cc8fd34d
ExtF8 <78138567+ExtF8@users.noreply.github.com>
2025-08-19 14:35:35 +0300

    Add basic screen with movie lookup by id

--- app/details.tsx
+++ app/details.tsx
@@ -1,9 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { useLocalSearchParams, } from 'expo-router';
+import { Movie } from '../types/Movies';
+import { getMovies } from '../utils/catalog';
 
 export default function DetailsScreen() {
+    const { id } = useLocalSearchParams<{ id: string }>();
+
+
+    const movies: Movie[] = getMovies();
+    const movie = movies.find(movie => movie.id === id);
     return (
         <View style={styles.container}>
-            <Text>This will be a details screen or modal</Text>
+            <Text>{movie ? movie.title : 'Movie not found'}</Text>
         </View>
     );
 }
